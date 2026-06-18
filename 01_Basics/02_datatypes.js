00b55ef406c4d1756f9f0118060f76d724037227
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-18 12:04:27 +0000

    datatypes and ECMA Standards

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// use the newer version of js 
+//alert(3 + 3) it should not be used here as we are using node js AND it will work on browsers etc.
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Mehreen")
+let name = "Mehreen"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age)
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object, sometimes treated as a bug 
