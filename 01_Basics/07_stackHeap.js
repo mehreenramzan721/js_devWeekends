7500c36e00e10b9eee61f0087b52d4872021051c
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-20 08:28:18 +0000

    stack and heap

---
+++ 01_Basics/07_stackHeap.js
@@ -0,0 +1,14 @@
+// let name="mehr"
+// let anotherName=name
+
+// anotherName="ali"
+// console.log(name)// mehr
+// console.log(anotherName)//ali
+
+let user1={
+    email="abc"
+}
+let user2=user1
+user2.email="xyz"
+console.log(user1.email)//xyz
+console.log(user2.email)//xyz
