75e296a4b55b7726c0f3d6ab1df8141c0e3246de
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-23 11:02:44 +0000

    scopes

---
+++ 03_Basics/03_scopes.js
@@ -0,0 +1,16 @@
+// let a=10
+// const b=20
+// var c=30
+// console.log(a)
+// console.log(b)
+// console.log(c)
+var c=900
+if(true){
+    let a=60
+    const b=70
+    var c=90  
+}
+
+// console.log(a)//ReferenceError: a is not defined
+//console.log(b)//ReferenceError: b is not defined
+console.log(c)//90
