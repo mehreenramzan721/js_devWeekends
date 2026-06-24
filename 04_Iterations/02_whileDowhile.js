882e4ec01fb4c92a6fac9452aae560159fd6914f
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-24 09:43:31 +0000

    while and do while

---
+++ 04_Iterations/02_whileDowhile.js
@@ -0,0 +1,19 @@
+// initialization
+// while(condition){
+
+//     incerement or decrement
+// }
+
+// let i=0
+// while(i<20){
+//     console.log(i)
+//     i=i+4
+// }
+
+
+// do while loop
+let i=0
+do{
+    console.log("1")
+    i++
+}while(i<5);//1 will be printed when i is smaller than 5
