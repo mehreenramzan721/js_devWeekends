53049c304246d70a3230251518d1abb0776f8228
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-06-24 09:21:43 +0000

    forloop

--- 04_Iterations/01_forLoops.js
+++ 04_Iterations/01_forLoops.js
@@ -2,7 +2,47 @@
 //     console.log(index);
 // }
 
-for(let index=0; index<10; index++){
-    if(i==5) console.log("Best 5")
-    console.log(index);
-}
+// for(let index=0; index<10; index++){
+//     if(i==5) console.log("Best 5")
+//     console.log(index);
+// }
+
+// for(let i =1; i<=5; i++){
+//     for(let j=2;j<5; j++){
+//         console.log(i +"*"+ j+"="+ i*j)
+//     }
+// }
+
+// // break and continue
+// for(let i=1;i<10; i++){
+//     if(i==5){
+//         console.log("get 5")
+//         break
+//     }
+//     console.log(i)
+// }
+// /* 1
+// 2
+// 3
+// 4
+// get 5*/
+// // note that the whole loop is exited in case of break
+
+// break and continue
+// for(let i=1;i<10; i++){
+//     if(i==5){
+//         console.log("get 5")
+//         continue
+//     }
+//     console.log(i)
+// }
+// /* 1
+// 2
+// 3
+// 4
+// get 5
+// 6
+// 7
+// 8
+// 9*/
+// // note that the loop runs just that iteration is skipped we didnt get 5 printed
