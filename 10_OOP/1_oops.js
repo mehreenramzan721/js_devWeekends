d6ea161f5ccfe3a048554ffd757d5aa60f085d62
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-01 10:02:06 +0000

    oops

---
+++ 10_OOP/1_oops.js
@@ -0,0 +1,26 @@
+// const user ={
+//     username: "mehreen",
+//     loginCount: 8,
+//     signedIn: true,
+//     getUserDetails: function(){
+//         //console.log("Got user details from database");
+//         // console.log(`Username: ${this.username}`);
+//         console.log(this);
+//     }
+// }
+// //console.log(user.username)
+// //console.log(user.getUserDetails());
+// // console.log(this);
+// function User(username, loginCount, isLoggedIn){
+//     this.username= username;
+//     this.loginCount= loginCount;
+//     this.isLoggedIn= isLoggedIn
+//     this.greeting= function(){
+//         console.log(`Welcome ${this.username}`);
+//     }
+//     return this
+// }
+// const userOne= new User("mehr", 12, true)
+// const userTwo= new User("ChaiAurCode", 11, false)
+// console.log(userOne.constructor);
+// //console.log(userTwo);
