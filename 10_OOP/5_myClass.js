2e253cac7f2f8eddc3490ecb03fb78431e16c00e
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-07-01 18:16:22 +0000

    myclass

---
+++ 10_OOP/5_myClass.js
@@ -0,0 +1,23 @@
+// after ES6
+
+class User{
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    encryptPassword(){
+        return `${this.password}abc`;
+    }
+        changeUsername(){
+            return `${this.username.toUpperCase()}`;
+    }
+}
+const chai = new User('chai', 'chai@gmail.com', '123')
+console.log(chai)
+/*
+User { username: 'chai', email: 'chai@gmail.com', password: '123' }
+*/
+
+console.log(chai.encryptPassword())//123abc
+console.log(chai.changeUsername())//CHAI
